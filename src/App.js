ec83244d11ae91166ee31790149c474cea3ba564
Sid Wighe <swighe@appnexus.com>
2019-03-11 15:55:19 -0400

    Add redux and test basic store functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { PureComponent } from 'react';
 import './App.css';
-
-class App extends Component {
+class App extends PureComponent {
   render() {
+    console.log("props : " + this.props)
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button onClick={this.props.onClick}>{this.props.value}</button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { createStore } from 'redux'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const counter = (state = 0, action) => {
+    console.log("action called : " + action);
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1
+        case 'DECREMENT':
+            return state - 1
+        default:
+            return state
+    }
+}
 
+var store = createStore(counter)
+const onClick = () => store.dispatch({type: 'INCREMENT'})
+const render = () => ReactDOM.render(<App onClick={onClick} value={store.getState()}/>, document.getElementById('root'))
+store.subscribe(render)
+
+render()
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
